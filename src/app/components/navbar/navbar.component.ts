97cb97009e605b0c2c016fb603d33c0591eb3e39
sasikanthreddy2311 <v.sasikanthreddy2311@gmail.com>
2024-07-13 18:11:41 +0530

    [nav bar layout]

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,12 +1,37 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
-  imports: [],
+  imports: [RouterModule, CommonModule],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.scss'
 })
 export class NavbarComponent {
-
+  navList: any = [{
+    name: 'Overview',
+    iconClass: 'bi bi-view-stacked'
+  },
+  {
+    name: 'Messages',
+    iconClass: 'bi bi-envelope'
+  },
+  {
+    name: 'Search',
+    iconClass: 'bi bi-search'
+  },
+  {
+    name: 'Filter',
+    iconClass: 'bi bi-filter-circle'
+  },
+  {
+    name: 'History',
+    iconClass: 'bi bi-clock-history'
+  },
+  {
+    name: 'My Account',
+    iconClass: 'bi bi-person-fill'
+  }]
 }
